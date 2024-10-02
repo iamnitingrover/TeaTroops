242950c76cc701ef35bc294a6ca7d314f746ca2c
Nitin Grover <nitingrover@live.com>
2024-10-02 21:55:35 +0530

    Fixing build bugs

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,26 +4,6 @@ import { Leaf, Instagram, Facebook, Twitter } from "lucide-react"
 export default function AboutUs() {
   return (
     <div className="flex flex-col min-h-screen">
-      {/* <header className="px-4 lg:px-6 h-14 flex items-center">
-        <Link className="flex items-center justify-center" href="/">
-          <Leaf className="h-6 w-6" />
-          <span className="ml-2 text-lg font-bold">Bloom & Brew</span>
-        </Link>
-        <nav className="ml-auto flex gap-4 sm:gap-6">
-          <Link className="text-sm font-medium hover:underline underline-offset-4" href="/">
-            Home
-          </Link>
-          <Link className="text-sm font-medium hover:underline underline-offset-4" href="/shop">
-            Shop
-          </Link>
-          <Link className="text-sm font-medium hover:underline underline-offset-4" href="/about">
-            About
-          </Link>
-          <Link className="text-sm font-medium hover:underline underline-offset-4" href="/contact">
-            Contact
-          </Link>
-        </nav>
-      </header> */}
       <main className="flex-1">
         <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 bg-[url('/placeholder.svg?height=600&width=1200&text=Our+Story')] bg-cover bg-center">
           <div className="container px-4 md:px-6">
